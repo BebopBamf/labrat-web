77447b6f94fdca370a5c016703b8f1ac5bd57bbb
Euan Mendoza <bebopbamf@effectfree.dev>
2024-03-28 16:35:04 +1100

    feat: added router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+import "./style.css";
+import App from "./App.vue";
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+const routes = [{ path: "/", component: HelloWorld }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
