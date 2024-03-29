7ec0caf00b4e46a26fe9e8ec9fbf7bee5f0f3b80
Euan Mendoza <bebopbamf@effectfree.dev>
2024-03-29 19:14:00 +1100

    feat(bootstrap): added bootstrap to project

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 
 import router from './router';
 
-import './style.css';
+//import './style.css';
+import '../scss/styles.scss';
 import App from './App.vue';
 
 createApp(App).use(router).mount('#app');
